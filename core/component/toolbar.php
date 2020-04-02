c44f9e682980916ee267eb20f2c4e0f319dd2934
Forx <forx.pro@protonmail.com>
2020-04-02 21:04:53 +0200

    Fix

--- core/component/toolbar.php
+++ core/component/toolbar.php
@@ -3,7 +3,7 @@
 function makeToolbar() {
     return '
         <div class="right-toolbar">
-                <button id="create-pack-button" class="button-main" type="submit" form="page-form" name="submit">Se déconnecter</button>
+                <button id="create-pack-button" class="button-main">Créer un paquet</button>
                 <img class="avatar-image" src="/res/avatar.png" alt="Avatar">
         </div>';
 }
