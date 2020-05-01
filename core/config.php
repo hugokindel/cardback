d5316cdc1ba735b0c724abb1d0aac8f51d0dae1a
Forx <forx.pro@protonmail.com>
2020-05-01 16:26:44 +0200

    Fix

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php namespace cardback;
 
 // URL du site.
-$serverUrl = "http://cardback.test";
+$serverUrl = "https://cardback.tech";
 
 // Informations générales du site.
 $serverTimezone = "Europe/Paris";
